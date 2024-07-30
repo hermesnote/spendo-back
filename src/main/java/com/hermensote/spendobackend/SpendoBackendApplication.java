4c61020cff0ce25c524f645475dd5d335ec82d35
hermesnote <hermesnote.blog@gmail.com>
2024-07-30 16:32:06 +0800

    Spring Boot initialed

---
+++ src/main/java/com/hermensote/spendobackend/SpendoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hermensote.spendobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hermensote/spendobackend/SpendoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermensote.spendobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
